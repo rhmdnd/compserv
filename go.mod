49440d74b47a4fbf59c6effb1f508c1ae51dc245
Lance Bragstad <lbragsta@redhat.com>
2022-05-26 20:34:11 -0500

    Add support for connecting to databases
    This commits adds plumbing to the configuration module to build
    connection strings. It also uses gorm to get a connection to the
    database in the compserv-server cmd.

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.16
 require (
 	github.com/aws/aws-sdk-go v1.44.22 // indirect
 	github.com/spf13/viper v1.12.0 // indirect
+	gorm.io/driver/postgres v1.3.6 // indirect
+	gorm.io/gorm v1.23.5 // indirect
 )
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,10 +1,15 @@
 package compserv
 
 import (
+	"encoding/base64"
 	"fmt"
 	"log"
 	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
 	"github.com/spf13/viper"
 )
 
@@ -49,4 +54,80 @@ func ParseConfig(configDir string) map[string]string {
 
 	log.Printf("Loaded configuration file: %s", viper.ConfigFileUsed())
 	return m
+}
+
+func GetDatabaseConnectionString(v map[string]string) string {
+	secret := getSecret(v["db_secret_arn"], v["secret_region"])
+	connectionString := fmt.Sprintf("host=%s user=%s dbname=%s password=%s port=%s", v["db_host"], v["db_username"], v["db_name"], secret, v["db_port"])
+	return connectionString
+}
+
+// This code comes from AWS Secret Manager
+func getSecret(secretName string, region string) string {
+	var secretString, decodedBinarySecret string
+
+	//Create a Secrets Manager client
+	sess, err := session.NewSession()
+	if err != nil {
+		// Handle session creation error
+		fmt.Println(err.Error())
+		return secretString
+	}
+	svc := secretsmanager.New(sess,
+		aws.NewConfig().WithRegion(region))
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId:     aws.String(secretName),
+		VersionStage: aws.String("AWSCURRENT"), // VersionStage defaults to AWSCURRENT if unspecified
+	}
+
+	// In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+	// See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+
+	result, err := svc.GetSecretValue(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case secretsmanager.ErrCodeDecryptionFailure:
+				// Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+				fmt.Println(secretsmanager.ErrCodeDecryptionFailure, aerr.Error())
+
+			case secretsmanager.ErrCodeInternalServiceError:
+				// An error occurred on the server side.
+				fmt.Println(secretsmanager.ErrCodeInternalServiceError, aerr.Error())
+
+			case secretsmanager.ErrCodeInvalidParameterException:
+				// You provided an invalid value for a parameter.
+				fmt.Println(secretsmanager.ErrCodeInvalidParameterException, aerr.Error())
+
+			case secretsmanager.ErrCodeInvalidRequestException:
+				// You provided a parameter value that is not valid for the current state of the resource.
+				fmt.Println(secretsmanager.ErrCodeInvalidRequestException, aerr.Error())
+
+			case secretsmanager.ErrCodeResourceNotFoundException:
+				// We can't find the resource that you asked for.
+				fmt.Println(secretsmanager.ErrCodeResourceNotFoundException, aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return secretString
+	}
+
+	// Decrypts secret using the associated KMS key.
+	// Depending on whether the secret is a string or binary, one of these fields will be populated.
+	if result.SecretString != nil {
+		secretString = *result.SecretString
+		return secretString
+	} else {
+		decodedBinarySecretBytes := make([]byte, base64.StdEncoding.DecodedLen(len(result.SecretBinary)))
+		len, err := base64.StdEncoding.Decode(decodedBinarySecretBytes, result.SecretBinary)
+		if err != nil {
+			fmt.Println("Base64 Decode Error:", err)
+			return secretString
+		}
+		decodedBinarySecret = string(decodedBinarySecretBytes[:len])
+		return decodedBinarySecret
+	}
 }
