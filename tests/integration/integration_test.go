09e845754719e23b968ade491299202da141e4b4
Lance Bragstad <lbragsta@redhat.com>
2022-06-23 10:49:32 -0500

    Add database integration tests for exercising migrations
    This commit adds an integration test that uses golang-migrate to upgrade
    and downgrade the database. This provides a skeleton we can use to write
    other tests that assert the behavior of the schemas we write.

--- tests/integration/integration_test.go
+++ tests/integration/integration_test.go
@@ -5,30 +5,86 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/assert"
 )
 
-func setUpDatabase(t *testing.T) *sql.DB {
+func getDatabaseConnection(t *testing.T) *sql.DB {
 	t.Helper()
 	// Generlize this so that it can be used to connect to any Postgres
 	// database to run tests.
 	connStr := "host=localhost user=dbadmin dbname=compliance password=secret port=5432 sslmode=disable"
 
-	db, err := sql.Open("postgres", connStr)
+	conn, err := sql.Open("postgres", connStr)
 	if err != nil {
 		msg := fmt.Sprintf("Unable to initialize connection to test database: %s", err)
 		t.Skip(msg)
 	}
-	if err := db.Ping(); err != nil {
+	if err := conn.Ping(); err != nil {
 		msg := fmt.Sprintf("Unable to establish connection to test database: %s", err)
 		t.Skip(msg)
 	}
 
-	// Finish adding migration logic.
-	return db
+	return conn
 }
 
-func TestSubject(t *testing.T) {
-	setUpDatabase(t)
-	t.Parallel()
+func getMigrationHelper(t *testing.T) *migrate.Migrate {
+	t.Helper()
+	conn := getDatabaseConnection(t)
+
+	driver, err := postgres.WithInstance(conn, &postgres.Config{})
+	if err != nil {
+		t.Fatal("Unable to initialize database driver for migrations")
+	}
+	m, err := migrate.NewWithDatabaseInstance("file://../../migrations", "postgres", driver)
+	if err != nil {
+		t.Fatal("Unable to initialize migrations")
+	}
+	return m
+}
+
+// Each test assumes the database is unmanaged. The test is responsible for
+// setting up the state it requires for its test logic. This keeps the
+// getMigrationHelper() method clean of any assumptions about what the tests
+// expect from it.
+func TestSubject(t *testing.T) { // nolint:paralleltest // database tests should not run in parallel
+	getMigrationHelper(t)
+	// m.Up()
+	// Test logic
+	// m.Down()
+}
+
+func TestMigration(t *testing.T) { // nolint:paralleltest // database tests should not run in parallel
+	m := getMigrationHelper(t)
+
+	version, dirty, err := m.Version()
+
+	expectedVersion := uint(0)
+	expectedState := false
+	assert.Equal(t, expectedVersion, version, "Database version mismatch: want %d but got %d", expectedVersion, version)
+	assert.Equal(t, expectedState, dirty, "Database state mismatch: want %t but got %t", expectedState, dirty)
+	// Currently, Version() doesn't return a typed error, but a generic one
+	// with a specific string (e.g., "no migration"). If this changes, we
+	// should update the test to check the error type returned and that
+	// it's what we expect.
+	assert.NotEmpty(t, err, "Collecting the version should return an error on an empty database")
+
+	// Upgrade the database and make sure all upgrades apply cleanly.
+	err = m.Up()
+	version, dirty, _ = m.Version()
+	expectedVersion = uint(1)
+	assert.Equal(t, expectedVersion, version, "Database version mismatch: want %d but got %d", expectedVersion, version)
+	assert.Equal(t, false, dirty, "Database state mismatch: want %t but got %t", false, dirty)
+	assert.Equal(t, err, nil, "Error upgrading the database: %s", err)
+
+	// Downgrade the database back to 0 and make sure all downgrades apply cleanly.
+	err = m.Down()
+	version, dirty, _ = m.Version()
+	expectedVersion = uint(0)
+	assert.Equal(t, expectedVersion, version, "Database version mismatch: want %d but got %d", expectedVersion, version)
+	assert.Equal(t, false, dirty, "Database state mismatch: want %t but got %t", false, dirty)
+	assert.Equal(t, err, nil, "Error downgrading the database: %s", err)
 }
