f34f6e889da12a0fbc0790997e9230190fa6e374
Lance Bragstad <lbragsta@redhat.com>
2022-09-09 12:10:32 -0500

    Implement basic server API
    This commit implements a single gRPC API endpoint for saving a result.
    Nothing is actually written to the database, but we're logging the
    compliance result the server gets so we can experiment with the API.

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -1,6 +1,11 @@
 package compserv
 
-import "gorm.io/gorm"
+import (
+	context "context"
+	"log"
+
+	"gorm.io/gorm"
+)
 
 type server struct {
 	UnimplementedComplianceServiceServer
@@ -9,4 +14,10 @@ type server struct {
 
 func NewServer(db *gorm.DB) *server { // nolint:revive,golint // returning a private struct from an exported fn is fine
 	return &server{database: db}
+}
+
+func (s *server) SetResult(ctx context.Context, result *ResultRequest) (*ResultResponse, error) {
+	log.Printf("%+v", result)
+	// Wire this up to the database and persist the result
+	return &ResultResponse{}, nil
 }
